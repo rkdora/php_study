f8a1c9994b40f074be20e28c1da62c55982ab8a1
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-05-16 08:45:59 +0900

    first step

--- slot.php
+++ slot.php
@@ -1,13 +1,24 @@
 <?php
+  $reel = "";
   $message =  "";
 
   session_start();
 
   if(isset($_POST['start'])){
-    $message =  "Hello world!";
+    $reel_1 = rand(1,3);
+    $reel_2 = rand(1,3);
+    $reel_3 = rand(1,3);
+    $reel = $reel_1."&nbsp;".$reel_2."&nbsp;".$reel_3;
+
+    if($reel_1 == $reel_2 && $reel_1 == $reel_3){
+      $message = "<font color=\"red\">YOU WIN!</font>";
+    }else { 
+      $message = "<font color=\"blue\">YOU LOSE!</font>";
+    }
   }
 
-?>
+
+echo<<<EOD
 
 <!DOCTYPE html>
   <html>
@@ -17,13 +28,19 @@
     </head>
 
     <body>
-      <h1>Slot Game</h1>
-      <p>
-        <form action="slot.php" method="post">
-          <input type="submit" name="start" value="start" />
-        </form>
-       <?php echo $message ?>
-      </p>
+      <div style = "text-align: center;">
+        <h1>Slot Game</h1>
+        <p>
+           <form action="slot.php" method="post">
+             <input type="submit" name="start" value="start" />
+          </form>
+        </p>
+        <p><font size="10">$reel</font></p>
+        <p>$message</p>
+      </div>
     </body>
   </html>
 
+EOD;
+
+?>
