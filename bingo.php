b308e689eb9d25b03abc5aa0b66fc23fb6bd969c
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-06-18 16:46:06 +0900

    bingo sheet html css

---
+++ bingo.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>BINGO!</title>
+  <style type="text/css">
+    body {
+      font-family: Arial, sans-serif;
+      text-align: center;
+      font-size: 16px;
+    }
+    .container {
+      margin: 0 auto;
+      width: 322px;
+    }
+    td, th {
+      width: 60px;
+      height: 45px;
+    }
+
+    td {
+      background: #F06292;
+      color: #fff;
+    }
+
+    th {
+      color: #F06292;
+      font-size: 22px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <table>
+      <tr>
+        <th>B</th><th>I</th><th>N</th><th>G</th><th>O</th>
+      </tr>
+      <tr> 
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
+        <td>1</td>
+      </tr>
+   </table>
+  </div>
+</body>
+</html>
