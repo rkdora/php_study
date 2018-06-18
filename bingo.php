b1a064f7c886d542a302617d043f0fd86b939fff
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-06-18 18:26:20 +0900

    bingo sheet shuffle number

--- bingo.php
+++ bingo.php
@@ -1,3 +1,21 @@
+<?php
+
+  $nums = [];
+
+  for ($i = 0; $i < 5; $i++) {
+    $col = range($i * 15 + 1, $i * 15 + 15);
+    shuffle($col);
+    $nums[$i] = array_slice($col, 0, 5);
+  }
+
+  $nums[2][2] = "FREE";
+
+  function h($s) {
+    return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+  }
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -35,13 +53,13 @@
       <tr>
         <th>B</th><th>I</th><th>N</th><th>G</th><th>O</th>
       </tr>
+      <?php for ($i = 0; $i < 5; $i++) : ?>
       <tr> 
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
-        <td>1</td>
+        <?php for ($j = 0; $j < 5; $j++) : ?>
+        <td><?= h($nums[$j][$i]); ?></td>
+       <?php endfor; ?>
       </tr>
+      <?php endfor; ?>
    </table>
   </div>
 </body>
